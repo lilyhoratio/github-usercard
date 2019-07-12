f817ff1dd1228301e0e1a73bc5f81b91560455e1
lilyhoratio <lilyhzhou@gmail.com>
2019-07-11 21:34:31 -0700

    refactored using append instead of appendchild

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -7,9 +7,35 @@
    data in order to use it to build your component function 
 
    Skip to Step 3.
+   Step 3: Create a function that accepts a single object as its only argument,
+          Using DOM methods and properties, create a component that will return the following DOM element:
+
+    <div class="card">
+      <img src={image url of user} />
+      <div class="card-info">
+        <h3 class="name">{users name}</h3>
+        <p class="username">{users user name}</p>
+        <p>Location: {users location}</p>
+        <p>Profile:  
+          <a href={address to users github page}>{address to users github page}</a>
+        </p>
+        <p>Followers: {users followers count}</p>
+        <p>Following: {users following count}</p>
+        <p>Bio: {users bio}</p>
+      </div>
+    </div>
 
    Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
+
+  Step 5: Now that you have your own card getting added to the DOM, either 
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          , manually find some other users' github handles, or use the list found 
+          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          Individual strings to the friendsArray below.
+          
+          Using that array, iterate over it, requesting data for each user, creating a new card for each
+          user, and adding that card to the DOM.
 */
 
 const githubHandle = "lilyhoratio";
@@ -25,24 +51,14 @@ axios
   .get(`https://api.github.com/users/${githubHandle}`)
   // Handles Success: here's where we get the results from server
   .then(res => {
-    console.log("response", res.data);
+    console.log("Response:", res.data);
     const githubUser = createCard(res.data);
     cards.appendChild(githubUser);
   })
   // Handles Failure
   .catch(error => {
-    console.log("API error", error);
+    console.log("API error:", error);
   });
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
 
 const followersArray = [
   "tetondan",
@@ -59,7 +75,7 @@ followersArray.forEach(follower => {
   });
 });
 
-//stretch 
+//stretch
 // v1 - from walkthrough
 // axios
 //   .get(`https://api.github.com/users/${githubHandle}/followers`)
@@ -78,37 +94,17 @@ followersArray.forEach(follower => {
 
 // v2 - from walkthrough
 
-axios.get(`https://api.github.com/users/${githubHandle}/followers`)
-  .then(res => res.data.slice(0, 3)) // limit to first 3 followers
+axios
+  .get(`https://api.github.com/users/${githubHandle}/followers`)
+  .then(res => res.data.slice(0, 3))
   .then(followers => {
-    followers.forEach( follower => {
-      axios.get(`https://api.github.com/users/${follower.login}`)
-        .then(res => {
-          const card = createCard(res.data);
-          cards.append(card);
-        })
-    })
-  })
-
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
+    followers.forEach(follower => {
+      axios.get(`https://api.github.com/users/${follower.login}`).then(res => {
+        const card = createCard(res.data);
+        cards.append(card);
+      });
+    });
+  });
 
 function createCard(githubProfile) {
   // create elements
@@ -125,18 +121,12 @@ function createCard(githubProfile) {
   const bio = document.createElement("p");
 
   // html structure
-  card.appendChild(img);
-  card.appendChild(cardInfo);
-  cardInfo.appendChild(name);
-  cardInfo.appendChild(username);
-  cardInfo.appendChild(location);
-  cardInfo.appendChild(profile);
-  // profile.appendChild(githubUrl)
-  cardInfo.appendChild(followers);
-  cardInfo.appendChild(following);
-  cardInfo.appendChild(bio);
+  card.append(img, cardInfo);
+  cardInfo.append(name, username, location, profile);
+  // profile.append(githubUrl) //move to after setting profile.textContent so that githubUrl isn't overridden
+  cardInfo.append(followers, following, bio);
 
-  // apply classes and tags
+  // apply classes
   card.classList.add("card");
   cardInfo.classList.add("card-info");
   name.classList.add("name");
@@ -144,16 +134,16 @@ function createCard(githubProfile) {
 
   // apply tags
   img.src = githubProfile.avatar_url;
-  githubUrl.href = `${githubProfile.html_url}`;
+  githubUrl.href = githubProfile.html_url;
 
   // apply text content
   name.textContent = githubProfile.name;
   username.textContent = githubProfile.login;
-  location.textContent = `Location: ${githubProfile.location}`;
+  location.textContent = `Location: ${githubProfile.location || "N/A"}`;
   profile.textContent = "Profile: "; //Overrides anchor tag children with text. From MDN - Setting textContent on a node removes all of the node's children and replaces them with a single text node with the given string value.
-  profile.appendChild(githubUrl);
+  profile.append(githubUrl);
   // profile.innerHTML = `Profile: <a href=${githubProfile.html_url}> ${githubProfile.html_url} </a>`
-  githubUrl.textContent = `${githubProfile.html_url}`;
+  githubUrl.textContent = githubProfile.html_url;
   followers.textContent = `Followers: ${githubProfile.followers}`;
   following.textContent = `Following: ${githubProfile.following}`;
   bio.textContent = `Bio: ${githubProfile.bio || "N/A"}`;
